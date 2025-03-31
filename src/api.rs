c82e1d54fb8c987905d0123f9052faafedfde0b8
Suffix Trie <email>
2025-03-31 19:48:55 +0200

    get houses in many cities

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,53 @@
-fn get_graphql_query(city_id: u64) -> String {
+fn get_graphql_query(city_id: CityId) -> String {
     format!(
         r#"{{ "operationName": "GetCategories", "variables": {{ "currentPage": 1, "filters": {{ "available_to_book": {{ "eq": "179" }}, "category_uid": {{ "eq": "Nw==" }}, "city": {{ "eq": "{}" }} }}, "pageSize": 100, "sort": {{ "available_startdate": "ASC" }} }}, "query": "query GetCategories($pageSize: Int!, $currentPage: Int!, $filters: ProductAttributeFilterInput!, $sort: ProductAttributeSortInput) {{ products( pageSize: $pageSize, currentPage: $currentPage, filter: $filters, sort: $sort ) {{ ...ProductsFragment, __typename }} }} fragment ProductsFragment on Products {{ sort_fields {{ options {{ label, value, __typename }}, __typename }}, aggregations {{ label, count, attribute_code, options {{ label, count, value, __typename }}, position, __typename }}, items {{ name, sku, city, url_key, available_to_book, available_startdate, next_contract_startdate, current_lottery_subscribers, building_name, finishing, living_area, no_of_rooms, resident_type, offer_text_two, offer_text, maximum_number_of_persons, type_of_contract, price_analysis_text, allowance_price, floor, basic_rent, lumpsum_service_charge, inventory, caretaker_costs, cleaning_common_areas, energy_common_areas, energy_label, minimum_stay, allowance_price, price_range {{ minimum_price {{ regular_price {{ value, currency, __typename }}, final_price {{ value, currency, __typename }}, __typename }}, maximum_price {{ regular_price {{ value, currency, __typename }}, final_price {{ value, currency, __typename }}, __typename }}, __typename }} , __typename }}, total_count, __typename }}" }}"#,
-        city_id
+        city_id.0
     )
 }
 
-pub async fn query_rooms() -> Result<(), reqwest::Error> {
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct CityId(u64);
+
+#[derive(Clone, Copy, PartialEq, Eq, derive_more::Display)]
+pub enum City {
+    Delft,
+    Eindhoven,
+    DenHaag,
+    Zoetermeer,
+    Rijswijk,
+    Rotterdam,
+}
+
+impl City {
+    pub fn id(&self) -> CityId {
+        match self {
+            City::Delft => CityId(26),
+            City::Eindhoven => CityId(29),
+            City::DenHaag => CityId(90),
+            City::Zoetermeer => CityId(6088),
+            City::Rijswijk => CityId(6224),
+            City::Rotterdam => CityId(25),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Holland2StayError {
+    #[error(transparent)]
+    ReqwestError(#[from] reqwest::Error),
+
+    #[error("Conversion error: {0}")]
+    ConversionError(String),
+}
+
+#[derive(derive_new::new, derive_more::Display)]
+#[display("{}: {}", city, name)]
+pub struct House {
+    pub name: String,
+    pub city: City,
+}
+
+pub async fn query_rooms(city: City) -> Result<Vec<House>, Holland2StayError> {
     let url = reqwest::Url::parse("https://api.holland2stay.com/graphql/")
         .expect("could not parse holland2stay api url");
     let client = reqwest::Client::new();
@@ -13,14 +55,14 @@ pub async fn query_rooms() -> Result<(), reqwest::Error> {
         .post(url)
         .header("User-Agent", "Mozilla/5.0")
         .header("Content-Type", "application/json")
-        .body(get_graphql_query(29))
+        .body(get_graphql_query(city.id()))
         .send()
         .await?
         .error_for_status()?
         .json::<serde_json::Value>()
         .await?;
 
-    fn get_rooms(data: &serde_json::Value) -> Option<Vec<&str>> {
+    fn get_rooms(data: &serde_json::Value, city: City) -> Option<Vec<House>> {
         Some(
             data.as_object()?
                 .get("data")?
@@ -31,16 +73,33 @@ pub async fn query_rooms() -> Result<(), reqwest::Error> {
                 .as_array()?
                 .into_iter()
                 .flat_map(|v| v.as_object()?.get("name")?.as_str())
+                .map(|s| House::new(s.to_string(), city))
                 .collect(),
         )
     }
 
-    let rooms = get_rooms(&response).ok_or_else(|| todo!())?;
-    for room in rooms {
-        println!("{room}");
-    }
+    get_rooms(&response, city).ok_or_else(|| {
+        Holland2StayError::ConversionError(
+            "Could not convert json response into list of available houses".to_string(),
+        )
+    })
+}
 
-    Ok(())
+pub async fn query_rooms_cities(cities: &[City]) -> Result<Vec<House>, Holland2StayError> {
+    let future_houses = cities
+        .into_iter()
+        .map(async |&city| query_rooms(city).await);
+
+    futures::future::join_all(future_houses)
+        .await
+        .into_iter()
+        .try_fold(
+            vec![],
+            |mut acc, houses| -> Result<Vec<House>, Holland2StayError> {
+                acc.append(&mut houses?);
+                Ok(acc)
+            },
+        )
 }
 
 #[cfg(test)]
@@ -49,12 +108,25 @@ mod tests {
 
     #[test]
     fn test_body() {
-        let body = get_graphql_query(29);
+        let body = get_graphql_query(City::Eindhoven.id());
         println!("{}", body);
     }
 
     #[tokio::test]
     async fn test_query_rooms() {
-        query_rooms().await.unwrap();
+        let cities = query_rooms(City::Rotterdam).await.unwrap();
+        for city in cities {
+            println!("{}", city.name);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_query_rooms_cities() {
+        let cities = query_rooms_cities(&[City::Rotterdam, City::Eindhoven])
+            .await
+            .unwrap();
+        for city in cities {
+            println!("{}", city);
+        }
     }
 }
